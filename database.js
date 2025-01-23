375fba707b58a746a214259154b6ffc058b59189
Gatito027 <dddanieeel@hotmail.com>
2025-01-23 17:37:40 -0600

    Database

---
+++ database.js
@@ -0,0 +1,14 @@
+const pgp = require('pg-promise')();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = pgp({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = db;
+
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./database.js');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json('¡Hi, World!, in express');
+});
+
+app.get('/database', async (req, res) => {
+    try {
+        const query = await db.any('SELECT * FROM usuarios');
+        res.json(query);
+    } catch (error) {
+        res.status(500).json('Error');
+        console.error(error);
+    }
+    
+});
+
+app.get('/test-connection', async (req, res) => {
+    try {
+        const result = await db.any('SELECT 1');
+        res.send('Conexión exitosa: ' + JSON.stringify(result));
+    } catch (error) {
+        res.send('Error en la conexión: ' + error.message);
+    }
 });
 
 app.listen(port, () => {
