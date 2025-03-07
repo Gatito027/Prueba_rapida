b124b610c255229bed2f64afb3c424a12fc5454b
Gatito027 <dddanieeel@hotmail.com>
2025-03-06 18:44:01 -0600

    Agregando metodo general

--- index.js
+++ index.js
@@ -357,6 +357,33 @@ app.get('/consulta-existecia-produccion', [
   }
 });
 
+app.get('/obtener-planeacion', async (req, res) => {
+  try {
+    const { token } = req.cookies;
+    logger.info('Obtencion de planeacion iniciada');
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) {
+          logger.warning('Token no valido');
+          return res.status(401).json({ msg: 'Token no valido' });
+        }
+        try {
+          const response = await db.query('SELECT * FROM ObtenerPlaneacion()');
+          res.json(response);
+        } catch (dbError) {
+          logger.error('Error en la consulta a la base de datos');
+          res.status(500).json({ msg: 'Error en la consulta a la base de datos'});
+        }
+      });
+    } else {
+      logger.warning('Se recibio un token no valido');
+      res.status(401).json({ msg: 'Token no valido' });
+    }
+  } catch (error) {
+    logger.error('Error grave al consultar la planeacion');
+    res.status(422).json({ msg: 'Algo ha fallado (⊙_⊙)？' });
+  }
+});
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
