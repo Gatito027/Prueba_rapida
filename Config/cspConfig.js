c047b20c461f308b066d05e6372903f875d78370
Gatito027 <dddanieeel@hotmail.com>
2025-03-13 19:40:26 -0600

    Configurando politicas parte 1

--- Config/cspConfig.js
+++ Config/cspConfig.js
@@ -3,14 +3,14 @@ const helmet = require('helmet');
 
 const cspConfig = helmet.contentSecurityPolicy({
     directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "example.com"],
-        styleSrc: ["'self'", "example.com"],
-        imgSrc: ["'self'", "example.com"],
-        connectSrc: ["'self'", "api.example.com"],
-        fontSrc: ["'self'", "fonts.gstatic.com"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
+        'default-src': [SELF],
+        'script-src': [SELF, 'https://example.com'],
+        'style-src': [SELF, 'https://example.com'],
+        'img-src': [SELF, 'https://example.com'],
+        'connect-src': [SELF, 'https://api.example.com'],
+        'font-src': [SELF, 'https://fonts.gstatic.com'],
+        'object-src': [NONE],
+        'upgrade-insecure-requests': [],
     },
 });
 
